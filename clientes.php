d79cac8f6df913559f6fee2ed7ba38a90e3548bb
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2018-10-24 11:04:21 -0300

    Criação do arquivo clientes.php

--- clientes.php
+++ clientes.php
@@ -2,48 +2,47 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Market M171 - Cidades</title>
+        <title>Market M171 - Clientes</title>
     </head>
     <body>
         <?php
             require_once 'menu.php';
         ?>
         
-        <h1 align="center">Cidades</h1>
+        <h1 align="center">Clientes</h1>
         
         <br><br><br>
         
-        <form action="controller/salvarCidades.php" method="GET" >
-            <label>Nome: </label>
-            <input type="text" name="txtNome" />
-            <input type="submit" value="Salvar"
-        </form>
+        <a href="frmCliente.php">
+            <button>Cadastrar novo cliente</button></a>
         
-        <hr>
+        <br><br>
         
         <table border="1">
             <tr>
                 <th>Código</th>
-                <th>Nome da Cidade</th>
+                <th>Nome do Cliente</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>Cidade</th>
                 <th>Editar</th>
                 <th>Excluir</th>
             </tr>
             
             <tr>
                 <td>01</td>
+                <td>João da Silva</td>
+                <td>111.222.333-44</td>
+                <td>(51) 98765-4321</td>
                 <td>Porto Alegre</td>
                 <td> <button>Editar</button> </td>
                 <td> <button>Excluir</button> </td>
             </tr>
-            
-            <tr>
-                <td>02</td>
-                <td>Viamão</td>
-                <td> <button>Editar</button> </td>
-                <td> <button>Excluir</button> </td>
-            </tr>
-            
         </table>
         
     </body>
 </html>
+
+
+
+
